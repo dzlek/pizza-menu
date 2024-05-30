f8f30dd96b5866655d13f6615bf55d8ae5a55626
dzlek <6894255@gmail.com>
2024-05-30 11:19:46 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@ function Pizza() {
     <div>
       <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
       <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+      <p>Tomato, mozarella, spinach, and ricotta cheeses</p>
     </div>
   );
 }
